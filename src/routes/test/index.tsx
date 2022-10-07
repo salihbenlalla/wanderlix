48aeaa9773f30cf4c6030bddeddc4bed00fa30dc
salihbenlalla <salihbenlalla@gmail.com>
2022-10-07 21:12:27 +0100

    add test route

---
+++ src/routes/test/index.tsx
@@ -0,0 +1,22 @@
+import { component$ } from '@builder.io/qwik';
+import type { DocumentHead } from '@builder.io/qwik-city';
+import { Link } from '@builder.io/qwik-city';
+
+export default component$(() => {
+  return (
+    <div>
+      <h1>
+        Test Page
+      </h1>
+
+      <p><img src='https://cdn.travel2.ml/a-boat-ride-2-1.jpg'/></p>
+      <Link class="mindblow" href="/flower">
+        Blow my mind 🤯
+      </Link>
+    </div>
+  );
+});
+
+export const head: DocumentHead = {
+  title: 'my image page',
+};
