e162db653af51309a89d61ae81df1abc6433a56b
salih benlalla <salihbenlalla@gmail.com>
2023-03-05 18:52:47 +0100

    start adding document head in blog layout

--- src/routes/blog/layout.tsx
+++ src/routes/blog/layout.tsx
@@ -1,13 +1,51 @@
 import { component$, Slot, useStyles$ } from "@builder.io/qwik";
+import { useDocumentHead } from "@builder.io/qwik-city";
+import type { DocumentHead } from "@builder.io/qwik-city";
 
 import styles from "./style.css?inline";
 
 export default component$(() => {
   useStyles$(styles);
 
+  const x = useDocumentHead();
+  console.log(x);
   return (
     <div class="post-content">
       <Slot />
     </div>
   );
 });
+
+interface EndpointData {
+  canonical: string;
+  slug: string;
+  frVersion: string;
+  enVersion: string;
+  authorName: string;
+  authorUrl: string;
+  tagName: string;
+  tagUrl: string;
+  breadcrumbs: string[];
+  title: string;
+  description: string;
+  image: string;
+  imageWidth: string;
+  imageHeight: string;
+  readDuration: string;
+  datePublished: string;
+  dateModified: string;
+  imageType: string;
+}
+
+export const head: DocumentHead<EndpointData> = ({ head }) => {
+  //   console.log(head);
+  return {
+    title: `MyCompany - ${head.title}`,
+    meta: [
+      {
+        name: "og:image",
+        content: head.frontmatter.image,
+      },
+    ],
+  };
+};
