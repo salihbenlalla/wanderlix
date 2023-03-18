439f5d8bdf9ac4303e90ade941747b4614bfa83d
salih benlalla <salihbenlalla@gmail.com>
2023-03-18 18:19:20 +0100

    ?

--- src/components/PostCommentSection/CommentsList/index.tsx
+++ src/components/PostCommentSection/CommentsList/index.tsx
@@ -35,7 +35,7 @@ export default component$<CommentsListProps>((props) => {
       <Resource
         value={props.comments}
         onPending={() => <p>Loading comments ...</p>}
-        onRejected={(error) => <p>failed to load comments {error}</p>}
+        onRejected={(error) => <p>failed to load comments {`${error}`}</p>}
         onResolved={(comments) => (
           <ul>
             {comments.map((commentProps) => (
