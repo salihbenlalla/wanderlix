8a3414ba03d12728a1b13f7ed5ea70ef0be04887
salih benlalla <salihbenlalla@gmail.com>
2024-09-14 20:52:33 +0100

    removed SSG from the authors route to reduce the amount of files in /dist

--- src/routes/(withSidebar)/author/[...authorName]/index.tsx
+++ src/routes/(withSidebar)/author/[...authorName]/index.tsx
@@ -2,14 +2,14 @@ import { component$ } from "@builder.io/qwik";
 import {
   type RequestEventLoader,
   routeLoader$,
-  type StaticGenerateHandler,
+  // type StaticGenerateHandler,
 } from "@builder.io/qwik-city";
 import { type PlatformCloudflarePages } from "@builder.io/qwik-city/middleware/cloudflare-pages";
 import PostsGrid from "~/components/PostsGrid";
 import AuthorPageHeader from "~/components/PostsGrid/AuthorPageHeader";
 import { getAuthorData } from "./getAuthorData";
 import GridHeader from "~/components/PostsGrid/GridHeader";
-import getAuthorParams from "./getAuthorParams";
+// import getAuthorParams from "./getAuthorParams";
 
 /**
  * Loads the author's data and posts from the database.
@@ -52,13 +52,13 @@ export default component$(() => {
   );
 });
 
-export const onStaticGenerate: StaticGenerateHandler = async (env) => {
-  // eslint-disable-next-line
-  const authorNames = await getAuthorParams(env);
-
-  return {
-    params: authorNames.map((authorName) => {
-      return { authorName };
-    }),
-  };
-};
+// export const onStaticGenerate: StaticGenerateHandler = async (env) => {
+//   // eslint-disable-next-line
+//   const authorNames = await getAuthorParams(env);
+//
+//   return {
+//     params: authorNames.map((authorName) => {
+//       return { authorName };
+//     }),
+//   };
+// };
