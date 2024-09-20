30c5a199a8db3a255dcc4fc8aab61f2e3b42c1a8
salih benlalla <salihbenlalla@gmail.com>
2024-09-20 17:47:45 +0100

    refactored some the images get handlers code and added /images to _routes.json

---
+++ src/routes/images/createGetHandler.ts
@@ -0,0 +1,48 @@
+import { type RequestEvent, type RequestHandler } from "@builder.io/qwik-city";
+import { type PlatformCloudflarePages } from "@builder.io/qwik-city/middleware/cloudflare-pages";
+
+// Common GET handler with a customizable asset formatter
+const createGetHandler = (
+  urlConverter: (url: URL) => string,
+): RequestHandler<PlatformCloudflarePages> => {
+  return async (event) => {
+    const { request } = event;
+
+    if (request.method !== "GET") {
+      event.send(new Response("Method Not Allowed", { status: 405 }));
+    }
+
+    try {
+      const response = await fetchImageAsset(event, urlConverter);
+      if (!response.ok) {
+        console.warn(
+          `Asset not found: ${request.url}, status: ${response.status}`,
+        );
+        event.send(new Response("Image Not Found", { status: 404 }));
+      }
+
+      const imageBlob = await response.blob();
+      const headers = {
+        "Content-Type": "image/webp",
+        "Cache-Control": "public, max-age=31536000",
+      };
+      event.send(new Response(imageBlob, { status: 200, headers }));
+    } catch (error) {
+      console.error(`Failed to fetch image: ${request.url}`, error);
+      event.send(new Response("Internal Server Error", { status: 500 }));
+    }
+  };
+};
+
+// Fetches the image from B2 cloud storage
+async function fetchImageAsset(
+  event: RequestEvent<PlatformCloudflarePages>,
+  urlConverter: (url: URL) => string,
+): Promise<Response> {
+  const url = new URL(event.request.url);
+  const b2Url = urlConverter(url);
+
+  return fetch(b2Url);
+}
+
+export default createGetHandler;
--- src/routes/images/thumbnail/[image]/index.ts
+++ src/routes/images/thumbnail/[image]/index.ts
@@ -1,57 +1,10 @@
-import { type RequestEvent, type RequestHandler } from "@builder.io/qwik-city";
-import { type PlatformCloudflarePages } from "@builder.io/qwik-city/middleware/cloudflare-pages";
+import createGetHandler from "~/routes/images/createGetHandler";
 
-export const onGet: RequestHandler<PlatformCloudflarePages> = async (event) => {
-  //   request.headers.set("Cache-Control", "no-cache");
-  const response = await handleRequest(event);
+export const onGet = createGetHandler(convertUrl);
 
-  event.send(response);
-};
-
-async function serveAsset(event: RequestEvent<PlatformCloudflarePages>) {
-  const url = new URL(event.request.url);
-
-  let cache: any, response: any;
-  try {
-    //@ts-ignore
-    cache = caches.default;
-    response = await cache.match(event.request);
-    if (!response) {
-      response = await fetchImage(url);
-      event.platform.ctx.waitUntil(cache.put(event.request, response.clone()));
-    }
-  } catch (error) {
-    response = await fetchImage(url);
-  }
-
-  return response;
-}
-
-async function handleRequest(event: RequestEvent<PlatformCloudflarePages>) {
-  if (event.request.method === "GET") {
-    let response = await serveAsset(event);
-    if (response.status > 399) {
-      response = new Response(response.statusText, { status: response.status });
-    }
-    return response;
-  }
-  return new Response("Method not allowed", { status: 405 });
-}
-
-const fetchImage = async (url: URL) => {
-  const cloudinaryURL = convertUrl(url);
-  let response = await fetch(cloudinaryURL);
-  const headers = {
-    "timing-allow-origin": "*",
-    "cache-control": "public, max-age=31536000",
-  };
-  response = new Response(response.body, { ...response, headers });
-  return response;
-};
-
-const convertUrl = (url: URL) => {
+function convertUrl(url: URL) {
   const imagePath = url.pathname.split("/");
   const imageName = imagePath[3];
 
   return `https://f004.backblazeb2.com/file/travel22/thumbnails/${imageName}`;
-};
+}
